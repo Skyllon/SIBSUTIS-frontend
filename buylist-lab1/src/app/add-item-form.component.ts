01a40d974250f909ecbca2c11a95ce7e3e36436d
Skyllon <skylon.sys@gmail.com>
2025-09-12 22:11:18 +0700

    add adding item form

---
+++ buylist-lab1/src/app/add-item-form.component.ts
@@ -0,0 +1,30 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { ProductItem } from './product-item.interface';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: '../index.html'
+})
+
+export class AddItemFormComponent {
+  @Output() itemAdd = new EventEmitter<Omit<ProductItem, 'id'>>()
+
+  cost: number  = 0
+  count: number = 0
+  name: string = ''
+
+  onConfirm(): void {
+    if (this.name.trim()) {
+      this.itemAdd.emit({
+        itemCost: this.cost,
+        itemCount: this.count,
+        itemName: this.name.trim(),
+        isItemPurchased: false
+      })
+    }
+
+    this.cost  = 0
+    this.count = 0
+    this.name  = ''
+  }
+}
