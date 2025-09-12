92ff951ae824ac70e0d3ecdf46ee00febda0766e
Skyllon <skylon.sys@gmail.com>
2025-09-12 22:16:02 +0700

    added ngModule configure

---
+++ buylist-lab1/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping-list.component';
+import { ShoppingItemComponent } from './shopping-item.component';
+import { AddItemFormComponent } from './add-item-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingItemComponent,
+    AddItemFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
