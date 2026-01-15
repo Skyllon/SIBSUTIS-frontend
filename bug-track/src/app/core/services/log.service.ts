2a8b9ac7fd6d93325d86cf32fe72106a5d005a64
Skyllon <skylon.sys@gmail.com>
2026-01-16 03:59:04 +0700

    all files was deleted

--- bug-track/src/app/core/services/log.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({ providedIn: 'root' })
-export class LogService {
-  log(action: string) {
-    console.log(`[LOG]: ${action} at ${new Date().toISOString()}`);
-  }
-}
